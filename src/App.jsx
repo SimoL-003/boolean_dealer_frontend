dfdbb6fd054d5781f42dfe80052029c1c5803197
SimoL-003 <simoneleuzzi2003@gmail.com>
2026-04-17 16:15:23 +0200

    feat: setup initial layout and add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CarDetails from "./pages/CarDetails";
+import BaseLayout from "./components/layouts/BaseLayout";
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<BaseLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="cars">
+            <Route path=":id" element={<CarDetails />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/organisms/Header.jsx
@@ -0,0 +1,33 @@
+import { Link, NavLink } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header className="sticky top-0 z-50 h-18 bg-graphite border-b border-iron">
+      <div className="container mx-auto h-full px-6 xl:px-10 max-w-7xl flex items-center justify-between">
+        {/* LOGO */}
+        <Link to="/" className="flex flex-col gap-px leading-none">
+          <span className="font-serif text-2xl font-medium text-white tracking-tight">
+            BoolAuto
+          </span>
+          <span className="text-xs font-light text-ash tracking-[0.2em] uppercase">
+            Your trusted dealer for used cars
+          </span>
+        </Link>
+
+        {/* NAV */}
+        <nav>
+          <ul className="flex items-center gap-8">
+            <li>
+              <a
+                href={import.meta.env.VITE_DASHBOARD_BACKEND_URL}
+                className="navlink"
+              >
+                Area riservata
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
